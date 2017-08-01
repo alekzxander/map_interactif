88c8628f0f50f9ee473130e316c97fda09d27c59
Alexandre Callens <alekzxand@gmail.com>
2017-08-01 14:34:28 +0400

    m

--- data.js
+++ data.js
@@ -13,7 +13,6 @@ map.on('click', function(){
   if (map.scrollWheelZoom.disable()){
     map.scrollWheelZoom.enable();
   }else if (map.scrollWheelZoom.enable()){
-    window.map.scrollWheelZoom.disable(),false;
   }
 });
 window.addEventListener('click',map.scrollWheelZoom.disable(),false);
@@ -33,27 +32,31 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {minZoom :10 , maxZoom : 
 
 
 
-$(document).ready(function(){
-  $('#case1').click(function(){
-   var atelier = L.marker([-20.897, 55.542]).addTo(map);
-   var prima =  L.marker([-20.897, 55.492]).addTo(map);
-  
+
 //pop-up 
-atelier.on('mouseover', function (e) {
-  L.popup().setLatLng(e.latlng)
-  .setContent('Passage sur une habitation')
-  .openOn(map);
-});
-atelier.on('mouseout', function (e) {
-  L.popup().closePopup();
-});
+// atelier.on('mouseover', function (e) {
+//   L.popup().setLatLng(e.latlng)
+//   .setContent('Passage sur une habitation')
+//   .openOn(map);
+// });
+// atelier.on('mouseout', function (e) {
+//   L.popup().closePopup();
+// });
 //pop-up
+$(document).ready(function(){
+    function habitat(){
+  $('#case1').click(function(){
+L.marker([-20.897, 55.542]).addTo(map);
+L.marker([-20.897, 55.492]).addTo(map);
+
 $('.chiffre').css('color', 'green')
    $('button').css('background-color', 'green')
 $('.trait').css('background-color', 'green') 
 $('.habitation').show();
 })
+}
 
+ function amenagement(){
   $('#case2').click(function(){
 L.marker([-21.027367, 55.268663]).addTo(map);
 L.marker([-20.905148, 55.500322]).addTo(map);
@@ -65,8 +68,8 @@ L.marker([-20.905148, 55.500322]).addTo(map);
     $('.trait').css('background-color', '#9ac9ea') 
  
   })
-});
-
+}
+function immobilier(){
 $('#case3').click(function(){
   L.marker([-20.931442, 55.298157]).addTo(map);
 L.marker([-20.932090, 55.294954]).addTo(map); 
@@ -74,7 +77,9 @@ $('.chiffre').css('color', '#56534e')
 $('button').css('background-color','#56534e')
 $('.trait').css('background-color', '#56534e') 
 })
+}
 //ingenierie
+function ingenierie(){
 $('#case4').click(function(){
     L.marker([-20.887, 55.442]).addTo(map);
 L.marker([-20.877, 55.452]).addTo(map); 
@@ -82,7 +87,11 @@ L.marker([-20.877, 55.452]).addTo(map);
  $('button').css('background-color', '#4baf9a') 
  $('.trait').css('background-color', '#4baf9a') 
 })
-
-
+}
+habitat();
+amenagement();
+immobilier();
+ingenierie();
+});lflfllfldfld
 
 
